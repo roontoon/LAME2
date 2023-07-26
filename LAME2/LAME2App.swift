5899b21eeba569f72c4b085ee244075522287dc5
Dan Rahenkamp <roontoon@me.com>
2023-07-26 18:24:18 -0400

    Initial Commit

---
+++ LAME2/LAME2App.swift
@@ -0,0 +1,17 @@
+//
+//  LAME2App.swift
+//  LAME2
+//
+//  Created by Roontoon on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct LAME2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
