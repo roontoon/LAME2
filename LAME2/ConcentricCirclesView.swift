a4018bd6900d6581f38e058df1fe3e1fcfe65cac
roontoon <dkamp@roontoon.com>
2023-08-27 22:32:08 -0400

    added gradiant to reflect current speed.

--- LAME2/ConcentricCirclesView.swift
+++ LAME2/ConcentricCirclesView.swift
@@ -1,92 +1,70 @@
-// Test view for joystick control
-import SwiftUI  // Import the SwiftUI framework
+import SwiftUI
 
-// Define a SwiftUI View struct named ConcentricCirclesView
 struct ConcentricCirclesView: View {
-    // Declare state variables for the black circle's position, speed, and direction
+    // State variables to hold the position of the black circle, speed, and direction
     @State private var blackCirclePosition: CGPoint? = nil
     @State private var speed: Double = 0
     @State private var direction: Double = 0
     
-    // Declare constants for the outer and inner circle radii
+    // Constants for the radius of the outer and inner circles
     private let outerCircleRadius: CGFloat = UIScreen.main.bounds.width * 2/3 / 2
     private var innerCircleRadius: CGFloat { outerCircleRadius * 0.25 }
-
-    // Define the body of the View
+    
     var body: some View {
-        // Calculate the diameter of the black circle
+        // Diameter of the black circle
         let blackCircleDiameter: CGFloat = innerCircleRadius * 2
 
-        // Use GeometryReader to get the dimensions of the parent view
+        // GeometryReader to get the dimensions of the parent view
         GeometryReader { geometry in
-            // Calculate the center point of the parent view
+            // Center point of the parent view
             let centerPoint = CGPoint(x: geometry.size.width / 2, y: geometry.size.height / 2)
 
-            // Create a ZStack to overlay multiple views
+            // ZStack to overlay multiple shapes and text
             ZStack {
-                // Create a VStack to display speed and direction
+                // VStack for displaying speed and direction
                 VStack {
                     Text("Speed: \(speed, specifier: "%.2f")")
                         .font(.title)
                     Text("Direction: \(direction, specifier: "%.2f")°")
                         .font(.title)
                 }
-                // Position the VStack at the top-center of the parent view
+                // Position the VStack
                 .position(x: geometry.size.width / 2, y: geometry.size.height / 4)
                 
-                // Create another ZStack for the circles and lines
+                // ZStack for drawing circles and handling gestures
                 ZStack {
-                    // Draw the outer solid circle
+                    // Outer circle
                     Circle()
                         .stroke(lineWidth: 2)
                         .frame(width: outerCircleRadius * 2, height: outerCircleRadius * 2)
 
-                    // Draw the inner solid circle
+                    // Inner circle
                     Circle()
                         .stroke(lineWidth: 2)
                         .frame(width: innerCircleRadius * 2, height: innerCircleRadius * 2)
 
-                    // Draw the dashed circle
-                    Circle()
-                        .stroke(style: StrokeStyle(lineWidth: 2, dash: [5]))
-                        .frame(width: (outerCircleRadius + innerCircleRadius), height: (outerCircleRadius + innerCircleRadius))
-
-                    // Draw the black circle
+                    // Custom shape for the gradient
+                    if let blackCirclePosition = blackCirclePosition { // Safely unwrap optional
+                        Sector(center: centerPoint, endPoint: blackCirclePosition, startAngle: CGFloat(direction - 45), endAngle: CGFloat(direction + 45))
+                            .fill(RadialGradient(gradient: Gradient(colors: [Color.green, Color.red]), center: .center, startRadius: innerCircleRadius, endRadius: sqrt(pow(blackCirclePosition.x - centerPoint.x, 2) + pow(blackCirclePosition.y - centerPoint.y, 2))))
+                    }
+                    
+                    // Black circle
                     Circle()
                         .fill()
                         .frame(width: blackCircleDiameter, height: blackCircleDiameter)
-                        .position(blackCirclePosition ?? centerPoint)
-
-
-                    Path { path in
-                        // Vertical bisecting line for the outer circle
-                        path.move(to: CGPoint(x: centerPoint.x, y: centerPoint.y - outerCircleRadius))
-                        path.addLine(to: CGPoint(x: centerPoint.x, y: centerPoint.y - innerCircleRadius))
-                        
-                        path.move(to: CGPoint(x: centerPoint.x, y: centerPoint.y + innerCircleRadius))
-                        path.addLine(to: CGPoint(x: centerPoint.x, y: centerPoint.y + outerCircleRadius))
-                        
-                        // Horizontal bisecting line for the outer circle
-                        path.move(to: CGPoint(x: centerPoint.x - outerCircleRadius, y: centerPoint.y))
-                        path.addLine(to: CGPoint(x: centerPoint.x - innerCircleRadius, y: centerPoint.y))
-                        
-                        path.move(to: CGPoint(x: centerPoint.x + innerCircleRadius, y: centerPoint.y))
-                        path.addLine(to: CGPoint(x: centerPoint.x + outerCircleRadius, y: centerPoint.y))
-                    }
-                    .stroke(style: StrokeStyle(lineWidth: 2))
-
-
+                        .position(blackCirclePosition ?? centerPoint) // Use the center as the default position
                 }
-                // Add drag gesture to the ZStack
+                // Gesture handling for the black circle
                 .gesture(
                     DragGesture(minimumDistance: 0)
                         .onChanged { value in
-                            // Calculate distance and angle from center
+                            // Calculate the new position, speed, and direction
                             let delta = CGPoint(x: value.location.x - centerPoint.x, y: value.location.y - centerPoint.y)
                             let distance = sqrt(pow(delta.x, 2) + pow(delta.y, 2))
                             let angle = atan2(delta.y, delta.x)
 
-                            // Set the black circle position
+                            // Bound the position within the outer circle
                             if distance > outerCircleRadius {
                                 let boundedX = cos(angle) * outerCircleRadius + centerPoint.x
                                 let boundedY = sin(angle) * outerCircleRadius + centerPoint.y
@@ -95,18 +73,15 @@ struct ConcentricCirclesView: View {
                                 blackCirclePosition = value.location
                             }
 
-                            // Calculate speed
-                            speed = min(Double(distance / outerCircleRadius), 1.0)
-
-                            // Calculate direction
-                            direction = Double(angle) * 180 / Double.pi - 90 + 180
+                            // Update speed and direction
+                            speed = min(Double(distance / outerCircleRadius) * 100, 100)
+                            direction = Double(angle) * 180 / Double.pi
                             if direction < 0 {
                                 direction += 360
-                            } else if direction > 360 {
-                                direction -= 360
                             }
                         }
                         .onEnded { _ in
+                            // Reset to initial state when the gesture ends
                             blackCirclePosition = centerPoint
                             speed = 0
                             direction = 0
@@ -121,7 +96,23 @@ struct ConcentricCirclesView: View {
     }
 }
 
-// Preview the ConcentricCirclesView
+// Custom shape for drawing the sector with gradient
+struct Sector: Shape {
+    var center: CGPoint
+    var endPoint: CGPoint
+    var startAngle: CGFloat
+    var endAngle: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: center)
+        path.addArc(center: center, radius: sqrt(pow(endPoint.x - center.x, 2) + pow(endPoint.y - center.y, 2)), startAngle: .degrees(Double(startAngle)), endAngle: .degrees(Double(endAngle)), clockwise: false)
+        path.closeSubpath()
+        return path
+    }
+}
+
+// Preview
 struct ConcentricCirclesView_Previews: PreviewProvider {
     static var previews: some View {
         ConcentricCirclesView()
